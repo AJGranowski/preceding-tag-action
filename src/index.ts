4c544a7959b6898b5629a6e21064c2238a0bc292
AJ Granowski <AJGranowski@users.noreply.github.com>
2025-09-02 14:36:51 -0500

    Parse inputs in the main entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import * as core from "@actions/core";
+import * as github from "@actions/github";
 import { Octokit } from "@octokit/rest";
 
-import { GitHubAPI } from "./GitHubAPI";
 import { fetchPrecedingTag } from "./fetchPrecedingTag";
+import { GitHubAPI } from "./GitHubAPI";
+import { Input } from "./Input";
 
 try {
     await (async (): Promise<void> => {
+        const input: Input = new Input(core.getInput, github.context);
         const octokit: Octokit = new Octokit({
             auth: core.getInput("token")
         });
-        const githubAPI = new GitHubAPI(octokit, {owner: "AJGranowski", repo: "preceding-tag-action"});
-        const precedingTag = await fetchPrecedingTag(githubAPI, "HEAD");
+
+        const githubAPI = new GitHubAPI(octokit, input.getRepository());
+        const precedingTag = await fetchPrecedingTag(githubAPI, input.getRef(), input.getFilter());
+
         if (precedingTag == null) {
             core.setOutput("tag", "");
         } else {
