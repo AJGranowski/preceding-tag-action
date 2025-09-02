c205a7626183ab732bb99412892280d137a22180
AJ Granowski <AJGranowski@users.noreply.github.com>
2025-09-02 12:58:22 -0500

    Update index/action

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
+import * as core from "@actions/core";
 import { Octokit } from "@octokit/rest";
 
 import { GitHubAPI } from "./GitHubAPI";
-import { PrecedingTag } from "./PrecedingTag";
+import { fetchPrecedingTag } from "./fetchPrecedingTag";
 
-const octokit: Octokit = new Octokit({});
-const githubAPI = new GitHubAPI(octokit, {owner: "AJGranowski", repo: "git-api-test"});
-const precedingTag = new PrecedingTag(githubAPI);
-
-console.log(await precedingTag.fetchPrecedingTag("HEAD"));
+try {
+    await (async (): Promise<void> => {
+        const octokit: Octokit = new Octokit({
+            auth: core.getInput("token")
+        });
+        const githubAPI = new GitHubAPI(octokit, {owner: "AJGranowski", repo: "preceding-tag-action"});
+        const precedingTag = await fetchPrecedingTag(githubAPI, "HEAD");
+        if (precedingTag == null) {
+            core.setOutput("tag", "");
+        } else {
+            core.setOutput("tag", precedingTag);
+        }
+    })();
+} catch (e) {
+    if (e instanceof Error) {
+        core.setFailed(e);
+    } else {
+        core.setFailed("An unknown error occurred.");
+    }
+}
