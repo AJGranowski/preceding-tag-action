b05fb720f3ee0ac9f72333c556b877e647b9c971
AJ Granowski <AJGranowski@users.noreply.github.com>
2025-09-17 01:38:17 -0500

    Increase fetchPrecedingTag coverage

--- test/fetchPrecedingTag.test.ts
+++ test/fetchPrecedingTag.test.ts
@@ -54,4 +54,146 @@ describe("fetchPrecedingTag", () => {
 
         expect(await fetchPrecedingTag(mockGithubAPI, "HEAD")).toBe("tag2");
     });
+
+    test("should return the latest commit date of equal distance tags", async () => {
+        const mockGithubAPI = mock<GitHubAPI>({
+            fetchAllTags: () => Promise.resolve(["tag1", "tag2"])
+        });
+
+        mockGithubAPI.fetchCommitDifference.mockResolvedValue(2);
+        mockGithubAPI.fetchCommitDate.mockImplementation((tag) => {
+            const committerCommitDate = ({
+                "tag1": "2025-09-10T03:37:12Z",
+                "tag2": "2025-09-09T06:23:06Z"
+            })[tag] ?? "1980-01-01T00:00:00Z";
+
+            return Promise.resolve({
+                committer: committerCommitDate
+            });
+        });
+
+        expect(await fetchPrecedingTag(mockGithubAPI, "HEAD")).toBe("tag1");
+
+        mockGithubAPI.fetchCommitDate.mockImplementation((tag) => {
+            const committerCommitDate = ({
+                "tag1": "2025-09-09T06:23:06Z",
+                "tag2": "2025-09-10T03:37:12Z"
+            })[tag];
+
+            return Promise.resolve({
+                committer: committerCommitDate
+            });
+        });
+
+        expect(await fetchPrecedingTag(mockGithubAPI, "HEAD")).toBe("tag2");
+
+        mockGithubAPI.fetchCommitDate.mockImplementation((tag) => {
+            const authorCommitDate = ({
+                "tag1": "2025-09-01T14:12:54Z",
+                "tag2": "2025-09-02T23:25:37Z"
+            })[tag];
+
+            return Promise.resolve({
+                author: authorCommitDate
+            });
+        });
+
+        expect(await fetchPrecedingTag(mockGithubAPI, "HEAD")).toBe("tag2");
+
+        mockGithubAPI.fetchCommitDate.mockImplementation((tag) => {
+            const authorCommitDate = ({
+                "tag1": "2025-09-02T23:25:37Z",
+                "tag2": "2025-09-01T14:12:54Z"
+            })[tag];
+
+            return Promise.resolve({
+                author: authorCommitDate
+            });
+        });
+
+        expect(await fetchPrecedingTag(mockGithubAPI, "HEAD")).toBe("tag1");
+
+        mockGithubAPI.fetchCommitDate.mockImplementation((tag) => {
+            const committerCommitDate = ({
+                "tag1": "2025-08-14T01:02:03Z",
+            })[tag];
+
+            return Promise.resolve({
+                committer: committerCommitDate
+            });
+        });
+
+        expect(await fetchPrecedingTag(mockGithubAPI, "HEAD")).toBe("tag1");
+
+        mockGithubAPI.fetchCommitDate.mockImplementation((tag) => {
+            const committerCommitDate = ({
+                "tag2": "2025-08-14T01:02:03Z",
+            })[tag];
+
+            return Promise.resolve({
+                committer: committerCommitDate
+            });
+        });
+
+        expect(await fetchPrecedingTag(mockGithubAPI, "HEAD")).toBe("tag2");
+    });
+
+    test("should return something for basically equal tags", async () => {
+        const mockGithubAPI = mock<GitHubAPI>({
+            fetchAllTags: () => Promise.resolve(["tag1", "tag2"])
+        });
+
+        mockGithubAPI.fetchCommitDifference.mockResolvedValue(3);
+        mockGithubAPI.fetchCommitDate.mockResolvedValue({});
+
+        expect(await fetchPrecedingTag(mockGithubAPI, "HEAD")).not.toBeNull();
+    });
+
+    test("should not include tags pointing to this ref if includeRef is false", async () => {
+        const mockGithubAPI = mock<GitHubAPI>({
+            fetchAllTags: () => Promise.resolve(["tag1", "tag2", "tag3"])
+        });
+
+        mockGithubAPI.fetchCommitDifference.mockImplementation((a, b) => {
+            const abTable: Record<string, number> = {
+                "tag1": -1,
+                "tag2": 0,
+                "tag3": 1
+            };
+
+            if (b === "HEAD") {
+                return Promise.resolve(abTable[a] ?? NaN);
+            } else if (a === "HEAD") {
+                return Promise.resolve(-(abTable[b] ?? NaN));
+            }
+
+            return Promise.resolve(NaN);
+        });
+
+        expect(await fetchPrecedingTag(mockGithubAPI, "HEAD")).toBe("tag3");
+    });
+
+    test("should include tags pointing to this ref if includeRef is true", async () => {
+        const mockGithubAPI = mock<GitHubAPI>({
+            fetchAllTags: () => Promise.resolve(["tag1", "tag2", "tag3"])
+        });
+
+        mockGithubAPI.fetchCommitDifference.mockImplementation((a, b) => {
+            const abTable: Record<string, number> = {
+                "tag1": -1,
+                "tag2": 0,
+                "tag3": 1
+            };
+
+            if (b === "HEAD") {
+                return Promise.resolve(abTable[a] ?? NaN);
+            } else if (a === "HEAD") {
+                return Promise.resolve(-(abTable[b] ?? NaN));
+            }
+
+            return Promise.resolve(NaN);
+        });
+
+        expect(await fetchPrecedingTag(mockGithubAPI, "HEAD", {includeRef: true})).toBe("tag2");
+    });
 });
