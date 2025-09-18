b9d6571574e89a077dab9a770db57494e120703f
AJ Granowski <AJGranowski@users.noreply.github.com>
2025-09-18 13:30:23 -0500

    Add fetchCommitDifference tests

--- test/GitHubAPI.test.ts
+++ test/GitHubAPI.test.ts
@@ -90,4 +90,149 @@ describe("GitHubAPI", () => {
             expect(githubAPI.fetchAllTags(new RegExp(""))).rejects.toThrowError();
         });
     });
+
+    describe("fetchCommitDifference", () => {
+        test("should return ahead_by if status is ahead", async () => {
+            const expectedAbsoluteDifference = 4;
+            const response = {
+                data: {
+                    status: "ahead",
+                    ahead_by: expectedAbsoluteDifference
+                }
+            };
+
+            const octokit = mock<Octokit>({
+                rest: {
+                    repos: {
+                        compareCommitsWithBasehead: (() => Promise.resolve(response)) as any
+                    }
+                }
+            });
+
+            const githubAPI = new GitHubAPI(octokit, defaultRepo);
+            const difference = await githubAPI.fetchCommitDifference("ref1", "ref2");
+            expect(difference).toBe(expectedAbsoluteDifference);
+        });
+
+        test("should return negative behind_by if status is behind", async () => {
+            const expectedAbsoluteDifference = 4;
+            const response = {
+                data: {
+                    status: "behind",
+                    behind_by: expectedAbsoluteDifference
+                }
+            };
+
+            const octokit = mock<Octokit>({
+                rest: {
+                    repos: {
+                        compareCommitsWithBasehead: (() => Promise.resolve(response)) as any
+                    }
+                }
+            });
+
+            const githubAPI = new GitHubAPI(octokit, defaultRepo);
+            const difference = await githubAPI.fetchCommitDifference("ref1", "ref2");
+            expect(difference).toBe(-expectedAbsoluteDifference);
+        });
+
+        test("should return 0 if status is identical", async () => {
+            const response = {
+                data: {
+                    status: "identical"
+                }
+            };
+
+            const octokit = mock<Octokit>({
+                rest: {
+                    repos: {
+                        compareCommitsWithBasehead: (() => Promise.resolve(response)) as any
+                    }
+                }
+            });
+
+            const githubAPI = new GitHubAPI(octokit, defaultRepo);
+            const difference = await githubAPI.fetchCommitDifference("ref1", "ref2");
+            expect(difference).toBe(0);
+        });
+
+        test("should return NaN if status is diverged", async () => {
+            const response = {
+                data: {
+                    status: "diverged",
+                    ahead_by: 3,
+                    behind_by: 2
+                }
+            };
+
+            const octokit = mock<Octokit>({
+                rest: {
+                    repos: {
+                        compareCommitsWithBasehead: (() => Promise.resolve(response)) as any
+                    }
+                }
+            });
+
+            const githubAPI = new GitHubAPI(octokit, defaultRepo);
+            const difference = await githubAPI.fetchCommitDifference("ref1", "ref2");
+            expect(difference).toBeNaN();
+        });
+
+        test("should throw error if ahead_by is negative", async () => {
+            const response = {
+                data: {
+                    status: "ahead",
+                    ahead_by: -1
+                }
+            };
+
+            const octokit = mock<Octokit>({
+                rest: {
+                    repos: {
+                        compareCommitsWithBasehead: (() => Promise.resolve(response)) as any
+                    }
+                }
+            });
+
+            const githubAPI = new GitHubAPI(octokit, defaultRepo);
+            expect(githubAPI.fetchCommitDifference("ref1", "ref2")).rejects.toThrowError();
+        });
+
+        test("should throw error if behind_by is negative", async () => {
+            const response = {
+                data: {
+                    status: "behind",
+                    behind_by: -1
+                }
+            };
+
+            const octokit = mock<Octokit>({
+                rest: {
+                    repos: {
+                        compareCommitsWithBasehead: (() => Promise.resolve(response)) as any
+                    }
+                }
+            });
+
+            const githubAPI = new GitHubAPI(octokit, defaultRepo);
+            expect(githubAPI.fetchCommitDifference("ref1", "ref2")).rejects.toThrowError();
+        });
+
+        test("should throw error on unknown status", async () => {
+            const response = {
+                data: {}
+            };
+
+            const octokit = mock<Octokit>({
+                rest: {
+                    repos: {
+                        compareCommitsWithBasehead: (() => Promise.resolve(response)) as any
+                    }
+                }
+            });
+
+            const githubAPI = new GitHubAPI(octokit, defaultRepo);
+            expect(githubAPI.fetchCommitDifference("ref1", "ref2")).rejects.toThrowError();
+        });
+    });
 });
